0f3a81bb4bebcc95fe296d4cb6436260b1efbd01
LukinewPL <kuzniarlukasz2010@gmail.com>
2026-03-21 21:52:24 +0100

    Initial Commit

---
+++ WordWise/WordWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordWiseApp.swift
+//  WordWise
+//
+//  Created by Łukasz on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
